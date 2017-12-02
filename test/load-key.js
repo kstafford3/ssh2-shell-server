3ca1845fb4066fcdaffba882e97ff938990cfce0
Kyle Stafford <kylejstafford@gmail.com>
2017-12-02 13:30:05 -0500

    Move public key to load-key.js
    Background:
    ----------
    Loading test key public keys relied on a relative path.
    This broke the tests when they were run from outside the project.
    
    Actions:
    -------
    * Copy the raw public key into load-key as a constant value.
    * Delete the test key directory and files.

--- test/load-key.js
+++ test/load-key.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
 const ssh2Utils = require('ssh2').utils;
 
+const RAW_PUBLIC_KEY = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCnGv1S0T53Ghw6sWUomFVgRKaPAkFeCFjdCN8PdA9HZ3VhfkIhbFS+FeP13mbAYLsOzR1cs0f3SU+IdbJ2M8gSYUx/6bnrR69K+iX7QBl9MZZ9Ei8q8UNo5h8Gsqx8ukljCEx+VFo/phhHZcTD/3CBT+EZvVR/QszySdqo61+zYBeNUf13w2piPRXT4hnTx8UKORtfOGYuFTB+IaQQue0V76o3RKX5M+q4TDg3IP2IdYEzk7x6/9tcGm1giyMBUm+iUjLYfIxCIam9Ec+XxDD3pCkpaAXyrlapAu5hktJFrEpz9+yCqHi20AH4jo6JU0xO1h/4Cxf+NEzjp2AUvmE/'
+
 function loadPublicKey() {
-  const rawPublicKey = fs.readFileSync('./test/test_keys/test_key.pub');
-  const parsedKey = ssh2Utils.parseKey(rawPublicKey);
+  const parsedKey = ssh2Utils.parseKey(RAW_PUBLIC_KEY);
   const publicKey = ssh2Utils.genPublicKey(parsedKey);
   return publicKey;
 }
